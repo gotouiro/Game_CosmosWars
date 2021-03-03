7a31f1ff4d15d27fbb550e6f82b6323d53fa5af7
gotouiro <yuuuu.tokyo2019.go@outlook.jp>
2021-03-03 18:13:46 +0900

    2021/03/03

---
+++ CosmosWars_Main/Assets/Scripts/All/GameSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class GameSystem : MonoBehaviour
+{
+    //---public------------------------------------------------------------------
+
+
+    //---private-----------------------------------------------------------------
+
+
+    //---protected---------------------------------------------------------------
+    /// <summary>
+    /// 初期化(UIの初期化もここで)
+    /// </summary>
+    protected abstract void Init();
+
+    /// <summary>
+    /// メイン処理
+    /// </summary>
+    protected abstract void Main();
+
+    /// <summary>
+    /// UIの設定・描画はここで
+    /// </summary>
+    protected abstract void UISystem();
+}
